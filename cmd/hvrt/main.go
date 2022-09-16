ed14f7ee4e7f06e416dc285a2fbc5a312137c4be
Ethan Estrada <ethan@misterfidget.com>
2022-09-15 22:24:03 -0600

    Add commandline parsing library.

--- cmd/hvrt/main.go
+++ cmd/hvrt/main.go
@@ -4,10 +4,31 @@ import (
 	"fmt"
 	"log"
 
+ "github.com/integrii/flaggy"
+
 	"github.com/eestrada/hvrt"
 )
 
 func main() {
+
+	// Declare variables and their defaults
+	var stringFlag = "defaultValue"
+
+	// Create the subcommand
+	subcommand := flaggy.NewSubcommand("subcommandExample")
+
+	// Add a flag to the subcommand
+	subcommand.String(&stringFlag, "f", "flag", "A test string flag")
+
+	// Add the subcommand to the parser at position 1
+	flaggy.AttachSubcommand(subcommand, 1)
+
+	// Parse the subcommand and all flags
+	flaggy.Parse()
+
+	// Use the flag
+	fmt.Println(stringFlag)
+
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
 	// the time, source file, and line number.
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eestrada/hvrt
 
 go 1.19
+
+require github.com/integrii/flaggy v1.5.2 // indirect

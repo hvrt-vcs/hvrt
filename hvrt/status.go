e58550eaa747e7ed63800300fc8f5485ea0f59c6
Ethan Estrada <ethan@misterfidget.com>
2022-09-26 22:48:55 -0600

    Start walking up the FS tree to find the root of the work tree.

--- hvrt/status.go
+++ hvrt/status.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	// "math/rand"
 	"os"
+	"path/filepath"
 	// "time"
 	// "modernc.org/sqlite"
 )
@@ -25,6 +26,33 @@ func init() {
 // 	}
 // }
 
+func GetWorkTreeRoot(start_dir string) string {
+
+	if start_dir == "" {
+		start_dir = "."
+	}
+
+	abs_path, err := filepath.Abs(start_dir)
+	if err != nil {
+		panic("Cannot determine the root of the work tree.")
+	}
+
+	cur_dir := abs_path
+	for cur_dir != "" {
+		cur_dir_fs := os.DirFS(cur_dir)
+		entries, _ := fs.ReadDir(cur_dir_fs, ".")
+		log.Println(entries)
+		log.Println(cur_dir)
+		if cur_dir == "/" {
+			cur_dir = ""
+		} else {
+			cur_dir = filepath.Dir(cur_dir)
+		}
+	}
+
+	return ""
+}
+
 type RepoStat struct {
 	DelPaths []string
 	ModPaths []string
@@ -33,6 +61,10 @@ type RepoStat struct {
 }
 
 func Status(repo_file, work_tree string) (RepoStat, error) {
+	real_work_tree := GetWorkTreeRoot(work_tree)
+	log.Println(real_work_tree)
+
+
 	stat := RepoStat{}
 	fileSystem := os.DirFS(work_tree)
 	log.Println(
@@ -41,6 +73,7 @@ func Status(repo_file, work_tree string) (RepoStat, error) {
 		work_tree,
 		fileSystem,
 	)
+	panic("whatever")
 
 	fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {

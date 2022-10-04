d6213176e258d72a56675dd74fa0689d94724e7c
Ethan Estrada <ethan@misterfidget.com>
2022-10-03 22:28:25 -0600

    Use hashes as primary keys.

--- hvrt/init.go
+++ hvrt/init.go
@@ -1,6 +1,7 @@
 package hvrt
 
 import (
+  "fmt"
   "database/sql"
   _ "modernc.org/sqlite"
 )
@@ -11,6 +12,15 @@ func init() {
   if err != nil {
     panic("Could not connect to sqlite database.")
   }
-  // sql_db.Exec()
+  defer sql_db.Close()
+  fmt.Println("Did we load the SQL DB?", sql_db)
+
+  initScript, err := sql_files.ReadFile("sql/sqlite3/init.sql")
+  if err != nil {
+    fmt.Println("We screwed up somehow:", err)
+    panic("Chickening out...")
+  }
+  fmt.Println("Did we load the init script?", initScript)
+  sql_db.Exec(string(initScript))
   // fmt.Println(sql_files)
 }
--- hvrt/sql/sqlite3/init.sql
+++ hvrt/sql/sqlite3/init.sql
@@ -4,20 +4,20 @@
 CREATE TABLE "vcs_version" (
 	"id"	INTEGER,
 	"version"	TEXT NOT NULL, -- semantic version
-	"created_at" INTEGER DEFAULT unixepoch(CURRENT_TIMESTAMP),
-	"modified_at" INTEGER DEFAULT unixepoch(CURRENT_TIMESTAMP),
+	"created_at" INTEGER DEFAULT strftime('%s','now'),
+	"modified_at" INTEGER DEFAULT strftime('%s','now'),
 	PRIMARY KEY("id" AUTOINCREMENT)
 )
 
 CREATE TABLE "tags" (
-	"id"	INTEGER,
 	"name"	TEXT NOT NULL,
 	"annotation"	TEXT,
 	"is_hidden" BOOLEAN NOT NULL DEFAULT FALSE,
 	"is_branch" BOOLEAN NOT NULL DEFAULT FALSE,
-	"created_at" INTEGER DEFAULT unixepoch(CURRENT_TIMESTAMP),
-	PRIMARY KEY("id" AUTOINCREMENT)
+	"created_at" INTEGER DEFAULT strftime('%s','now'),
+	PRIMARY KEY("name")
 )
+
 
 -- It is theoretically possible to have a single tree shared between multiple
 -- commits. This can happen under the following conditions: the file IDs and
@@ -26,20 +26,14 @@ CREATE TABLE "tags" (
 -- on file ID hash (as a UTF8 hex string) and blob ID hash (as a UTF8 hex
 -- string), in that order.
 CREATE TABLE "trees" (
-	"id"	INTEGER,
-	"hash_algo"	TEXT NOT NULL,
 	"hash"	TEXT NOT NULL,
-	UNIQUE("hash_algo", "hash")
-	PRIMARY KEY("id" AUTOINCREMENT)
+	"hash_algo"	TEXT NOT NULL,
+	PRIMARY KEY("hash", "hash_algo")
 )
 
-CREATE INDEX tree_hash_algos_idx ON trees("hash_algo");
-CREATE INDEX tree_hashes_idx ON trees("hash");
-
 CREATE TABLE "commits" (
-	"id"	INTEGER,
-	"hash_algo"	TEXT NOT NULL,
 	"hash"	TEXT NOT NULL,
+	"hash_algo"	TEXT NOT NULL,
 	"time"	INTEGER NOT NULL, -- seconds since the unix epoch in UTC
 
 	-- "tz_offset" is used to shift "time" by the given UTC offset (mostly for
@@ -47,129 +41,136 @@ CREATE TABLE "commits" (
 	-- purposes "localtime" is treated as UTC (since we have no way of knowing
 	-- what it actually is).
 	"tz_offset"	INTEGER,  -- Should this be taken into account for hashing?
+
 	"message"	TEXT NOT NULL,
 	"committer"	TEXT NOT NULL,
 	"author"	TEXT NOT NULL,
-	"tree_id"	INTEGER NOT NULL,
-	UNIQUE("hash_algo", "hash")
-	PRIMARY KEY("id" AUTOINCREMENT)
-	FOREIGN KEY ("tree_id") REFERENCES "trees" ("id") ON DELETE RESTRICT
+	"tree_hash"	TEXT NOT NULL,
+	"tree_hash_algo"	TEXT NOT NULL,
+	PRIMARY KEY("hash", "hash_algo")
+	FOREIGN KEY ("tree_hash", "tree_hash_algo") REFERENCES "trees" ("hash", "hash_algo") ON DELETE RESTRICT
 )
 
-CREATE INDEX commit_hash_algos_idx ON commits("hash_algo");
-CREATE INDEX commit_hashes_idx ON commits("hash");
 CREATE INDEX commit_times_idx ON commits("time");
 
 CREATE TABLE "commit_tags" (
-	"id"	INTEGER,
-
 	-- associated branch names/ids are NOT considered when hashing commits
-	"commit_id"	INTEGER NOT NULL,
-	"tag_id"	INTEGER NOT NULL,
-	UNIQUE("commit_id", "tag_id")
-	PRIMARY KEY("id" AUTOINCREMENT)
-	FOREIGN KEY ("commit_id") REFERENCES "commits" ("id") ON DELETE RESTRICT
-	FOREIGN KEY ("tag_id") REFERENCES "tags" ("id") ON DELETE CASCADE
+	"commit_hash"	TEXT NOT NULL,
+	"commit_hash_algo"	TEXT NOT NULL,
+	"tag_name"	TEXT NOT NULL,
+	PRIMARY KEY("commit_hash", "commit_hash_algo", "tag_name")
+	FOREIGN KEY ("commit_hash", "commit_hash_algo") REFERENCES "commits" ("hash", "hash_algo") ON DELETE RESTRICT
+	FOREIGN KEY ("tag_name") REFERENCES "tags" ("name") ON DELETE CASCADE
 )
 
-CREATE INDEX tag_commits_idx ON commit_tags("commit_id");
-CREATE INDEX commit_tags_idx ON commit_tags("tag_id");
+CREATE INDEX tag_commits_idx ON commit_tags("commit_hash", "commit_hash_algo");
+CREATE INDEX commit_tags_idx ON commit_tags("tag_name");
 
+-- If a given commit has no parents, it is a root commit.
+-- If a given commit points to parent that does not exist, it is because of a shallow checkout.
 CREATE TABLE "commit_parents" (
 	"id"	INTEGER,
-	"commit_id"	INTEGER NOT NULL,
-	"parent_id"	INTEGER NOT NULL,
-	"parent_type"	TEXT CHECK("parent_type" IN ('regular', 'merge', 'cherry_pick', 'replay', 'reorder')) NOT NULL DEFAULT 'regular',
+	"commit_hash"	TEXT NOT NULL,
+	"commit_hash_algo"	TEXT NOT NULL,
+	"parent_hash"	TEXT NOT NULL,
+	"parent_hash_algo"	TEXT NOT NULL,
+	"parent_type"	TEXT CHECK("parent_type" IN ('regular', 'merge', 'cherry_pick', 'replay', 'reorder')) NOT NULL,
 	"order"	INTEGER NOT NULL,
-	UNIQUE("commit_id", "parent_id")
-	PRIMARY KEY("id" AUTOINCREMENT)
-	FOREIGN KEY ("commit_id") REFERENCES "commits" ("id") ON DELETE RESTRICT
-	FOREIGN KEY ("parent_id") REFERENCES "commits" ("id") ON DELETE RESTRICT
+	PRIMARY KEY("commit_hash", "commit_hash_algo", "parent_hash", "parent_hash_algo")
+	FOREIGN KEY ("commit_hash", "commit_hash_algo") REFERENCES "commits" ("hash", "hash_algo") ON DELETE RESTRICT
+	FOREIGN KEY ("parent_hash", "parent_hash_algo") REFERENCES "commits" ("hash", "hash_algo") ON DELETE RESTRICT
 )
 
-CREATE INDEX commit_parents_commit_id_idx ON commit_parents("commit_id");
-CREATE INDEX commit_parents_parent_id_idx ON commit_parents("parent_id");
+CREATE INDEX commit_parents_commit_id_idx ON commit_parents("commit_hash", "commit_hash_algo");
+CREATE INDEX commit_parents_parent_id_idx ON commit_parents("parent_hash", "parent_hash_algo");
 
--- Bundles can be used in place of squashing to preserve commit history.
+-- Bundles can be used in place of squashing to preserve commit history. They
+-- are ephemeral metadata that is not hashed into the merkle tree.
 CREATE TABLE "bundles" (
 	"id"	INTEGER,
+
+	-- Do bundles really _need_ a name? perhaps it isn't necessary to make it NOT NULL and UNIQUE.
 	"name"	TEXT NOT NULL,
 	"message"	TEXT,
-	"created_at" INTEGER DEFAULT unixepoch(CURRENT_TIMESTAMP),
-	"modified_at" INTEGER DEFAULT unixepoch(CURRENT_TIMESTAMP),
-	UNIQUE("name")
-	PRIMARY KEY("id" AUTOINCREMENT)
+	"created_at" INTEGER DEFAULT strftime('%s','now'),
+	"modified_at" INTEGER DEFAULT strftime('%s','now'),
+	UNIQUE ("name")
+	PRIMARY KEY ("id" AUTOINCREMENT)
 )
 
 CREATE TABLE "bundle_commits" (
 	"id"	INTEGER,
 	"bundle_id"	INTEGER NOT NULL,
-	"commit_id"	INTEGER NOT NULL,
-	"created_at" INTEGER DEFAULT unixepoch(CURRENT_TIMESTAMP),
-	PRIMARY KEY("id" AUTOINCREMENT)
+	"commit_hash"	TEXT NOT NULL,
+	"commit_hash_algo"	TEXT NOT NULL,
+	"created_at" INTEGER DEFAULT strftime('%s','now'),
 
-	-- A commit should never be part of more than one bundle.
-	UNIQUE("commit_id") -- UNIQUE automatically creates an index
+	-- A commit should never be part of more than one bundle, so make that the primary key.
+	PRIMARY KEY ("commit_hash", "commit_hash_algo")
+	FOREIGN KEY ("commit_hash", "commit_hash_algo") REFERENCES "commits" ("hash", "hash_algo") ON DELETE RESTRICT
 	FOREIGN KEY ("bundle_id") REFERENCES "bundles" ("id") ON DELETE CASCADE
-	FOREIGN KEY ("commit_id") REFERENCES "commits" ("id") ON DELETE RESTRICT
 )
 
 CREATE INDEX bundle_commits_bundle_id_idx ON bundle_commits("bundle_id");
 
 CREATE TABLE "file_ids" (
-	"id"	INTEGER,
-	"hash_algo"	TEXT NOT NULL,
 	"hash"	TEXT NOT NULL,
+	"hash_algo"	TEXT NOT NULL,
 	"path"	TEXT NOT NULL,
-	UNIQUE("hash_algo", "hash")
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("hash", "hash_algo")
+
+	-- ALthough the `UNIQUE` constraint below is not needed internally to this
+	-- table, it is required for the `tree_members` table to ensure that two
+	-- separate file IDs with the same path value are not added to a single tree.
+	-- See the constraints on the `tree_members` table for more details.
+	UNIQUE ("hash", "hash_algo", "path")
 )
 
 -- If a given file_id has no parents, it was created "ex nihilo".
 CREATE TABLE "file_id_parents" (
-	"id"	INTEGER,
-	"file_id"	INTEGER NOT NULL,
-	"parent_id"	INTEGER NOT NULL,
+	"file_id_hash"	TEXT NOT NULL,
+	"file_id_hash_algo"	TEXT NOT NULL,
+	"parent_hash"	TEXT NOT NULL,
+	"parent_hash_algo"	TEXT NOT NULL,
 	"order"	INTEGER NOT NULL,
-	UNIQUE("file_id", "parent_id")
-	PRIMARY KEY("id" AUTOINCREMENT)
-	FOREIGN KEY ("file_id") REFERENCES "file_ids" ("id")
-	FOREIGN KEY ("parent_id") REFERENCES "file_ids" ("id")
+	PRIMARY KEY ("file_id_hash", "file_id_hash_algo", "parent_hash", "parent_hash_algo")
+	FOREIGN KEY ("file_id_hash", "file_id_hash_algo") REFERENCES "file_ids" ("hash", "hash_algo") ON DELETE RESTRICT
+	FOREIGN KEY ("parent_hash", "parent_hash_algo") REFERENCES "file_ids" ("hash", "hash_algo") ON DELETE RESTRICT
 )
 
-CREATE INDEX file_id_parents_file_id_idx ON file_id_parents("file_id");
-CREATE INDEX file_id_parents_parent_id_idx ON file_id_parents("parent_id");
-
 CREATE TABLE "blobs" (
-	"id"	INTEGER,
-	"hash_algo"	TEXT NOT NULL,
 	"hash"	TEXT NOT NULL,
+	"hash_algo"	TEXT NOT NULL,
 	"byte_length"	INTEGER NOT NULL,
-	UNIQUE("hash_algo", "hash")
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY ("hash", "hash_algo")
 )
 
 CREATE TABLE "tree_members" (
-	"id"	INTEGER,
-	"tree_id"	INTEGER NOT NULL,
-	"file_id"	INTEGER NOT NULL,
-	"blob_id"	INTEGER NOT NULL,
-	UNIQUE("tree_id", "file_id")
-	PRIMARY KEY("id" AUTOINCREMENT)
-	FOREIGN KEY ("tree_id") REFERENCES "trees" ("id")
-	FOREIGN KEY ("file_id") REFERENCES "file_ids" ("id")
-	FOREIGN KEY ("blob_id") REFERENCES "blobs" ("id")
+	"tree_hash"	TEXT NOT NULL,
+	"tree_hash_algo"	TEXT NOT NULL,
+	"file_id_hash"	TEXT NOT NULL,
+	"file_id_hash_algo"	TEXT NOT NULL,
+	"path"	TEXT NOT NULL,
+	"blob_hash"	TEXT NOT NULL,
+	"blob_hash_algo"	TEXT NOT NULL,
+	UNIQUE ("tree_hash", "tree_hash_algo", "path")
+	PRIMARY KEY ("tree_hash", "tree_hash_algo", "file_id_hash", "file_id_hash_algo")
+	FOREIGN KEY ("tree_hash", "tree_hash_algo") REFERENCES "trees" ("hash", "hash_algo") ON DELETE RESTRICT
+	FOREIGN KEY ("file_id_hash", "file_id_hash_algo", "path") REFERENCES "file_ids" ("hash", "hash_algo", "path") ON DELETE RESTRICT
+	FOREIGN KEY ("blob_hash", "blob_hash_algo") REFERENCES "blobs" ("hash", "hash_algo") ON DELETE RESTRICT
 )
 
-CREATE INDEX tree_members_tree_id_idx ON tree_members("tree_id");
-CREATE INDEX tree_members_file_id_idx ON tree_members("file_id");
-CREATE INDEX tree_members_blob_id_idx ON tree_members("blob_id");
+CREATE INDEX tmemb_trees_idx ON tree_members("tree_hash", "tree_hash_algo");
+CREATE INDEX tmemb_file_ids_idx ON tree_members("file_id_hash", "file_id_hash_algo", "path");
+CREATE INDEX tmemb_file_id_hashes_idx ON tree_members("file_id_hash");
+CREATE INDEX tmemb_paths_idx ON tree_members("path");
+CREATE INDEX tmemb_blobs_idx ON tree_members("blob_hash", "blob_hash_algo");
 
 -- Each blob chunk is compressed individually, so that we can decompress them
 -- individually later when streaming them.
 CREATE TABLE "blob_chunks" (
-	"id"	INTEGER,
-	"blob_id"	INTEGER NOT NULL,
+	"blob_hash"	TEXT NOT NULL,
+	"blob_hash_algo"	TEXT NOT NULL,
 
 	-- there is no simple constraint to check that start and end byte indices
 	-- don't overlap between sibling blob chunks, so this will likely need to be
@@ -184,10 +185,10 @@ CREATE TABLE "blob_chunks" (
 	"end_byte"	INTEGER NOT NULL,
 	"compression_algo"	TEXT, -- may be NULL to indicate uncompressed data
 	"data"	BLOB NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-	FOREIGN KEY ("blob_id") REFERENCES "blobs" ("id")
+	PRIMARY KEY("blob_hash", "blob_hash_algo", "start_byte")
+	FOREIGN KEY ("blob_hash", "blob_hash_algo") REFERENCES "blobs" ("hash", "hash_algo") ON DELETE RESTRICT
 )
 
-CREATE INDEX blob_chunks_blob_id_idx ON blob_chunks("blob_id");
-CREATE INDEX blob_chunks_start_byte_idx ON blob_chunks("start_byte");
-CREATE INDEX blob_chunks_end_byte_idx ON blob_chunks("end_byte");
+CREATE INDEX blb_chnks_blob_idx ON blob_chunks("blob_hash", "blob_hash_algo");
+CREATE INDEX blb_chnks_start_byte_idx ON blob_chunks("start_byte");
+CREATE INDEX blb_chnks_end_byte_idx ON blob_chunks("end_byte");

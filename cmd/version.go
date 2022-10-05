d9864d038128c344c4c6d921461bd5071fe9eee2
Ethan Estrada <ethan@misterfidget.com>
2022-10-05 16:33:26 -0600

    Add flag to `version` for printing out a semantic version.
    This is the same string that is written into the database, minus a 
    trailing newline which is added on the CLI for readability.

--- cmd/version.go
+++ cmd/version.go
@@ -8,7 +8,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var versionFlags = struct {
+	SemanticVersion bool
+}{
+	SemanticVersion: false,
+}
+
 func init() {
+	versionCmd.Flags().BoolVarP(&versionFlags.SemanticVersion, "semantic-version", "s", false, "Print out a semantic version instead of friendly version")
+
 	rootCmd.AddCommand(versionCmd)
 }
 
@@ -17,6 +25,12 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Havarti",
 	Long:  `All software has versions. This is Havarti's.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(hvrt.FormattedVersion)
+		var version string
+		if versionFlags.SemanticVersion {
+			version = hvrt.SemanticVersion
+		} else {
+			version = hvrt.FormattedVersion
+		}
+		fmt.Println(version)
 	},
 }

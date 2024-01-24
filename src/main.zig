97f5e7ce5390164cbd976c91e5b10df4929b3c35
Ethan Estrada <ethan@misterfidget.com>
2024-01-24 10:15:42 -0600

    Import sqlite.h in zig main
    Parse args in zig main. Still trying to figure out this language.

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,51 @@
 const std = @import("std");
+const sqlite3 = @cImport({
+    @cInclude("sqlite3.h");
+});
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // Args parsing from here: https://ziggit.dev/t/read-command-line-arguments/220/7
+    // Get allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Parse args into string array (error union needs 'try')
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    try bw.flush(); // don't forget to flush!
+    // Get and print them!
+    std.debug.print("There are {d} args:\n", .{args.len});
+    for (args) |arg| {
+        std.debug.print("  {s}\n", .{arg});
+    }
+
+    // var db: *sqlite3.sqlite3 = null;
+    const db_path = args[1];
+
+    std.debug.print("what is db_path: {s}", .{db_path});
+
+    // const rc = sqlite3.sqlite3_open(db_path, db);
+    // defer sqlite3.sqlite3_close(db);
+
+    // if (rc) {
+    //     std.debug.print("Can't open database: {s}\n", db_path);
+    //     return 1;
+    // }
+
+    // // stdout is for the actual output of your application, for example if you
+    // // are implementing gzip, then only the compressed bytes should be sent to
+    // // stdout, not any debugging messages.
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    // try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {

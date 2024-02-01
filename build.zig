e59c2d4e1f120cb2f110a264fe6067f2c3573b3c
Ethan Estrada <ethan@misterfidget.com>
2024-02-01 09:53:05 -0600

    Revert "Add support for multiple compile targets"
    This reverts commit a157688c21dfe4b0cc661163e3a232a6aab9cec7.

--- build.zig
+++ build.zig
@@ -1,19 +1,5 @@
 const std = @import("std");
 
-const targets: []const std.Target.Query = &.{
-    .{ .cpu_arch = .aarch64, .os_tag = .linux },
-    .{ .cpu_arch = .aarch64, .os_tag = .macos },
-    .{ .cpu_arch = .arm, .os_tag = .linux },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
-    .{ .cpu_arch = .x86_64, .os_tag = .windows },
-    // .{ .cpu_arch = .aarch64, .os_tag = .freebsd },
-    // .{ .cpu_arch = .x86_64, .os_tag = .freebsd },
-    // .{ .cpu_arch = .x86_64, .os_tag = .macos },
-    // .{ .cpu_arch = .x86_64, .os_tag = .netbsd },
-    // .{ .cpu_arch = .x86_64, .os_tag = .openbsd },
-};
-
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
@@ -29,54 +15,43 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    for (targets) |t| {
-        const exe = b.addExecutable(.{
-            .name = "hvrt",
-            // In this case the main source file is merely a path, however, in more
-            // complicated build scripts, this could be a generated file.
-            .root_source_file = .{ .path = "src/main.zig" },
-            .target = b.resolveTargetQuery(t),
-            .optimize = optimize,
-        });
+    const exe = b.addExecutable(.{
+        .name = "hvrt",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
-        // This declares intent for the executable to be installed into the
-        // standard location when the user invokes the "install" step (the default
-        // step when running `zig build`).
-        b.installArtifact(exe);
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
 
-        const target_output = b.addInstallArtifact(exe, .{
-            .dest_dir = .{
-                .override = .{
-                    // .custom = try t.zigTriple(b.allocator),
-                    .custom = t.zigTriple(b.allocator) catch unreachable,
-                },
-            },
-        });
-        b.getInstallStep().dependOn(&target_output.step);
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
 
-        // // This *creates* a Run step in the build graph, to be executed when another
-        // // step is evaluated that depends on it. The next line below will establish
-        // // such a dependency.
-        // const run_cmd = b.addRunArtifact(exe);
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
 
-        // // By making the run step depend on the install step, it will be run from the
-        // // installation directory rather than directly from within the cache directory.
-        // // This is not necessary, however, if the application depends on other installed
-        // // files, this ensures they will be present and in the expected location.
-        // run_cmd.step.dependOn(b.getInstallStep());
-
-        // // This allows the user to pass arguments to the application in the build
-        // // command itself, like this: `zig build run -- arg1 arg2 etc`
-        // if (b.args) |args| {
-        //     run_cmd.addArgs(args);
-        // }
-
-        // // This creates a build step. It will be visible in the `zig build --help` menu,
-        // // and can be selected like this: `zig build run`
-        // // This will evaluate the `run` step rather than the default, which is "install".
-        // const run_step = b.step("run", "Run the app");
-        // run_step.dependOn(&run_cmd.step);
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{

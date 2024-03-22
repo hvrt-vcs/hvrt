590352319d97bca8bcdfe62df7d5842b45ea1493
Ethan Estrada <ethan@misterfidget.com>
2024-03-21 23:04:53 -0500

    Add `ChunkRef` type

--- src/ds.zig
+++ src/ds.zig
@@ -134,7 +134,15 @@ pub const FileId = struct {
 
 pub const Blob = struct {
     hash_key: HashKey,
-    chunks: []*Chunk,
+
+    /// Within a single blob, chunkref byte ranges must not overlap.
+    chunks: []ChunkRef,
+};
+
+pub const ChunkRef = struct {
+    start_byte: u64,
+    end_byte: u64,
+    chunk: *Chunk,
 };
 
 pub const Chunk = struct {

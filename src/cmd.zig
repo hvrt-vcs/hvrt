ef873b680fc9801c048ebb1d0660e3be25782356
Ethan Estrada <ethan@misterfidget.com>
2024-04-18 23:42:28 -0500

    Add some args parsing

--- src/cmd.zig
+++ src/cmd.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const parseArgs = @import("parse_args.zig").parseArgs;
+const parse_args = @import("parse_args.zig");
 const init = @import("init.zig").init;
 const add = @import("add.zig").add;
 const commit = @import("commit.zig").commit;
@@ -8,34 +8,44 @@ const commit = @import("commit.zig").commit;
 /// It is the responsibility of the caller of `internalMain` to deallocate and
 /// deinit args and alloc, if necessary.
 pub fn internalMain(alloc: std.mem.Allocator, args: []const [:0]const u8) !void {
-    const parsed_args = try parseArgs(alloc, args);
+    const parsed_args = try parse_args.parseArgs(alloc, args);
     defer parsed_args.deinit();
 
     if (args.len > 1) {
         const sub_cmd = args[1];
-        const repo_dir = if (args.len > 2) try std.fs.realpathAlloc(alloc, args[2]) else try std.process.getCwdAlloc(alloc);
-        defer alloc.free(repo_dir);
-        const repo_dirZ = try alloc.dupeZ(u8, repo_dir);
-        defer alloc.free(repo_dirZ);
+        const cmd_enum_opt = std.meta.stringToEnum(parse_args.Command, sub_cmd);
+        if (cmd_enum_opt) |cmd_enum| {
+            const repo_dir = if (args.len > 2) try std.fs.realpathAlloc(alloc, args[2]) else try std.process.getCwdAlloc(alloc);
+            defer alloc.free(repo_dir);
+            const repo_dirZ = try alloc.dupeZ(u8, repo_dir);
+            defer alloc.free(repo_dirZ);
 
-        if (std.mem.eql(u8, sub_cmd, "init")) {
-            try init(alloc, repo_dirZ);
-        } else if (std.mem.eql(u8, sub_cmd, "add")) {
-            // FIXME: this doesn't work if they don't pass a repo path. Need to
-            // do args parsing at some point.
-            const files = if (args.len > 3) args[3..] else args[2..];
+            switch (cmd_enum) {
+                .init => {
+                    try init(alloc, repo_dirZ);
+                },
+                .add => {
+                    // FIXME: this doesn't work if they don't pass a repo path.
+                    // Need to do real args parsing at some point.
+                    const files = if (args.len > 3) args[3..] else args[2..];
 
-            try add(alloc, repo_dirZ, files);
-        } else if (std.mem.eql(u8, sub_cmd, "commit")) {
-            try commit(alloc, repo_dirZ, "Dummy message");
-        } else if (std.mem.eql(u8, sub_cmd, "mv")) {
-            try notImplemented(sub_cmd);
-        } else if (std.mem.eql(u8, sub_cmd, "cp")) {
-            try notImplemented(sub_cmd);
-        } else if (std.mem.eql(u8, sub_cmd, "rm")) {
-            try notImplemented(sub_cmd);
+                    try add(alloc, repo_dirZ, files);
+                },
+                .commit => {
+                    try commit(alloc, repo_dirZ, "Dummy message");
+                },
+                .mv => {
+                    try notImplemented(sub_cmd);
+                },
+                .cp => {
+                    try notImplemented(sub_cmd);
+                },
+                .rm => {
+                    try notImplemented(sub_cmd);
+                },
+            }
         } else {
-            std.log.err("Unknown sub-command given: {s}.\n", .{sub_cmd});
+            std.log.err("Unknown sub-command given: {s}\n", .{sub_cmd});
             return error.ArgumentError;
         }
     } else {
--- src/parse_args.zig
+++ src/parse_args.zig
@@ -1,8 +1,19 @@
 const std = @import("std");
 
+pub const Command = enum {
+    add,
+    commit,
+    cp,
+    init,
+    mv,
+    rm,
+};
+
 pub const Args = struct {
     alloc: std.mem.Allocator,
     arena_ptr: *std.heap.ArenaAllocator,
+
+    command: Command,
 
     pub fn deinit(self: Args) void {
         self.arena_ptr.deinit();
@@ -13,12 +24,20 @@ pub const Args = struct {
 // Caller must call `deinit` on returned `Args` object when it is no longer
 // needed, otherwise memory will be leaked.
 pub fn parseArgs(alloc: std.mem.Allocator, args: []const [:0]const u8) !Args {
-    _ = args;
     const arena_ptr = try alloc.create(std.heap.ArenaAllocator);
     arena_ptr.* = std.heap.ArenaAllocator.init(alloc);
+
+    for (args[1..], 1..) |arg, i| {
+        _ = arg;
+        _ = i;
+    }
+
+    // FIXME: actually parse this from somewhere
+    const cmd = .init;
 
     return Args{
         .alloc = alloc,
         .arena_ptr = arena_ptr,
+        .command = cmd,
     };
 }

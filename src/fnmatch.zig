56ec40a4bcae8d0a6c2d7657e444db9115115cfa
Ethan Estrada <ethan@misterfidget.com>
2024-05-12 22:16:26 -0500

    Continue `fnmatch` parser/matcher

--- src/fnmatch.zig
+++ src/fnmatch.zig
@@ -11,20 +11,17 @@ pub const FnMatchState = union(enum) {
     char_class: []const u21,
     exact: u21,
     match: void,
+    start: void,
     wild_many: void,
     wild_single: void,
 };
 
 pub const StateNode = struct {
     state: FnMatchState,
-    next_states: []*StateNode,
+    next_states: std.ArrayList(*StateNode),
 };
 
-pub const FnMatcher = struct {
-    initial_state: *StateNode,
-    alloc: std.mem.Allocator,
-    arena: std.heap.ArenaAllocator,
-
+const FnMatchParser = struct {
     const ParseState = enum(u21) {
         char_class_open = '[',
         escape = '\\',
@@ -34,27 +31,110 @@ pub const FnMatcher = struct {
         wild_single = '?',
     };
 
-    pub fn init(alloc: std.mem.Allocator, pattern: []const u8) !FnMatcher {
-        var char_class = std.ArrayList(u21).init(alloc);
-        _ = &char_class;
-        var utf_view = try std.unicode.Utf8View.init(pattern);
-        var utf8_iter = utf_view.iterator();
+    pub fn parse(gpa: std.mem.Allocator, arena: std.mem.Allocator, pattern: []const u8) !*StateNode {
 
-        var parse_state = ParseState.literal;
-        _ = &parse_state;
-        while (utf8_iter.nextCodepoint()) |rune| {
+        // Create a local arena for the parsing process. On success, all data
+        // is copied to the arena passed into the function.
+        var larena_state = std.heap.ArenaAllocator.init(gpa);
+        defer larena_state.deinit();
+        const larena = larena_state.allocator();
+
+        var initial_state = try arena.create(StateNode);
+        initial_state.* = .{ .state = .{ .start = void{} }, .next_states = std.ArrayList(*StateNode).init(larena) };
+        _ = &initial_state;
+        var current_state = initial_state;
+        _ = &current_state;
+
+        // If the rune doesn't parse to a possible state, it must
+        // be a unicode code point, meaning it is a rune literal.
+        var utf8_view = try std.unicode.Utf8View.init(pattern);
+        var utf8_iter = utf8_view.iterator();
+
+        var current_rune = utf8_iter.nextCodepoint() orelse return error.ParseError;
+        var parse_state = std.meta.intToEnum(ParseState, current_rune) catch ParseState.literal;
+        while (utf8_iter.nextCodepoint()) |next_rune| {
             switch (parse_state) {
-                .char_class_open => unreachable,
-                .escape => unreachable,
-                .literal => {
-                    const state = std.meta.intToEnum(ParseState, rune) catch {};
-                    _ = state;
+                .char_class_open => {
+                    const state_ptr = try arena.create(StateNode);
+                    state_ptr.* = try parseCharclass(larena, current_state, current_rune);
+                    current_state = state_ptr;
                 },
-                .range => unreachable,
-                .wild_many => unreachable,
-                .wild_single => unreachable,
+                .escape => {
+                    // Force the next iteration to treat the `prior_rune` as a
+                    // literal, no matter what the rune it actually is.
+                    parse_state = ParseState.literal;
+                    current_rune = next_rune;
+                    continue;
+                },
+                .literal => {
+                    const state_ptr = try arena.create(StateNode);
+                    state_ptr.* = try parseLiteral(larena, current_state, current_rune);
+                    current_state = state_ptr;
+                },
+                .wild_many => {
+                    const state_ptr = try arena.create(StateNode);
+                    state_ptr.* = try parseWildMany(larena, current_state, current_rune);
+                    current_state = state_ptr;
+                },
+                .wild_single => {
+                    const state_ptr = try arena.create(StateNode);
+                    state_ptr.* = try parseWildSingle(larena, current_state, current_rune);
+                    current_state = state_ptr;
+                },
+                // Ranges should only be possible within char classes
+                .range => return error.ParseError,
             }
+
+            // If the rune doesn't parse to a possible state, it must be a
+            // unicode code point, meaning it is a rune literal.
+            parse_state = std.meta.intToEnum(ParseState, next_rune) catch ParseState.literal;
+            current_rune = next_rune;
         }
+    }
+
+    pub fn parseLiteral(larena: std.mem.Allocator, from_state: *StateNode, rune: u21) !StateNode {
+        _ = rune; // autofix
+        _ = from_state; // autofix
+        _ = larena; // autofix
+    }
+
+    pub fn parseCharclass(larena: std.mem.Allocator, from_state: *StateNode, rune: u21) !StateNode {
+        _ = rune; // autofix
+        _ = from_state; // autofix
+        _ = larena; // autofix
+    }
+
+    pub fn parseRange(larena: std.mem.Allocator, from_state: *StateNode, rune: u21) !StateNode {
+        _ = rune; // autofix
+        _ = from_state; // autofix
+        _ = larena; // autofix
+    }
+
+    pub fn parseWildMany(larena: std.mem.Allocator, from_state: *StateNode, rune: u21) !StateNode {
+        _ = rune; // autofix
+        _ = from_state; // autofix
+        _ = larena; // autofix
+    }
+
+    pub fn parseWildSingle(larena: std.mem.Allocator, from_state: *StateNode, rune: u21) !StateNode {
+        _ = rune; // autofix
+        _ = from_state; // autofix
+        _ = larena; // autofix
+    }
+};
+
+pub const FnMatcher = struct {
+    initial_state: *StateNode,
+    alloc: std.mem.Allocator,
+    arena: std.heap.ArenaAllocator,
+
+    pub fn init(gpa: std.mem.Allocator, pattern: []const u8) !FnMatcher {
+        var arena_state = std.heap.ArenaAllocator.init(gpa);
+        errdefer arena_state.deinit();
+        const arena = arena_state.allocator();
+
+        const initial_state = try FnMatchParser.parse(gpa, arena, pattern);
+        return .{ .initial_state = initial_state, .arena = arena_state };
     }
 
     pub fn deinit(self: *FnMatcher) void {

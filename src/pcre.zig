1a722d75fe8c9cbafe55ac70de3ceb3ab0068553
Ethan Estrada <ethan@misterfidget.com>
2024-05-16 11:10:38 -0500

    Add comments pcre2

--- src/pcre.zig
+++ src/pcre.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
 const c = @import("c.zig");
 
+/// A basic wrapper around raw pcre2 API. See docs here:
+/// https://www.pcre.org/current/doc/html/pcre2api.html
 pub const Matcher = struct {
+    // Eventually wrap allocator in a pcre2 context.
+    alloc: std.mem.Allocator,
     matcher: *c.pcre2_code_8,
 
-    pub fn compile(regex: []const u8) !Matcher {
+    /// Must call `free` on returned object, or memory will be leaked.
+    pub fn compile(alloc: std.mem.Allocator, regex: []const u8) !Matcher {
         const options: u32 = 0;
         var errorcode: c_int = 0;
         var erroroffset: usize = 0;
@@ -13,7 +18,7 @@ pub const Matcher = struct {
         const matcher_opt = c.pcre2_compile_8(regex.ptr, regex.len, options, &errorcode, &erroroffset, context);
 
         if (matcher_opt) |matcher| {
-            return .{ .matcher = matcher };
+            return .{ .matcher = matcher, .alloc = alloc };
         } else {
             var error_msg_buf: [1024 * 4]u8 = undefined;
             const error_msg_size = c.pcre2_get_error_message_8(errorcode.*, &error_msg_buf, error_msg_buf.len);
@@ -30,6 +35,7 @@ pub const Matcher = struct {
         c.pcre2_code_free_8(self.matcher);
     }
 
+    /// Docs: https://www.pcre.org/current/doc/html/pcre2api.html#SEC39
     pub fn dfa_match(self: Matcher, string: []const u8, start_offset: ?usize) bool {
         const options: u32 = 0;
         const context: ?*c.pcre2_compile_context_8 = null;

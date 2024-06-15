8c135ae52bca1782f08e166d9589df1c41ed41dd
Ethan Estrada <ethan@misterfidget.com>
2024-06-14 21:38:43 -0500

    Add initial version of `addFile` functions

--- src/add.zig
+++ src/add.zig
@@ -22,7 +22,7 @@ const chunk_size = 1024 * 4;
 
 /// It is the responsibility of the caller of `add` to deallocate and
 /// deinit alloc, repo_path, and files, if necessary.
-pub fn add(alloc: std.mem.Allocator, repo_path: [:0]const u8, files: []const [:0]const u8) !void {
+pub fn add(alloc: std.mem.Allocator, repo_path: []const u8, files: []const []const u8) !void {
     const abs_repo_path = try std.fs.realpathAlloc(alloc, repo_path);
     defer alloc.free(abs_repo_path);
 
@@ -216,7 +216,7 @@ pub const FileAdder = struct {
         self.repo_root.close();
     }
 
-    pub fn init(repo_path: [:0]const u8) !FileAdder {
+    pub fn init(repo_path: []const u8) !FileAdder {
         var fba_buf: [std.fs.max_path_bytes * 2]u8 = undefined;
         var fba_state = std.heap.FixedBufferAllocator.init(&fba_buf);
         const alloc = fba_state.allocator();
@@ -258,6 +258,148 @@ pub const FileAdder = struct {
             .blob_chunk_stmt = blob_chunk_stmt,
             .chunk_stmt = chunk_stmt,
         };
+    }
+
+    pub fn addFiles(self: FileAdder, scratch: std.mem.Allocator, files: []const []const u8) !void {
+        const tx = try sqlite.Transaction.init(self.wt_db, "add_cmd");
+        errdefer tx.rollback() catch unreachable;
+
+        const chunk_buffer = try scratch.alloc(u8, chunk_size);
+        defer scratch.free(chunk_buffer);
+
+        const fifo_buf = try scratch.alloc(u8, fifo_buffer_size);
+        defer scratch.free(fifo_buf);
+
+        for (files) |file| {
+            try self.addFile(scratch, tx, file, chunk_buffer, fifo_buf);
+        }
+
+        try tx.commit();
+    }
+
+    pub fn addFile(self: FileAdder, alloc: std.mem.Allocator, tx: sqlite.Transaction, file: []const u8, chunk_buffer: []u8, fifo_buf: []u8) !void {
+        const file_sp = try tx.createSavepoint("add_single_file");
+        errdefer file_sp.rollback() catch unreachable;
+
+        var chunk_buf_stream = std.io.fixedBufferStream(&chunk_buffer);
+        var fifo = std.fifo.LinearFifo(u8, .Slice).init(fifo_buf);
+
+        if (std.fs.path.isAbsolute(file)) {
+            log.err("File to add to stage must be relative path: {s}", .{file});
+            return error.AbsoluteFilePath;
+        }
+
+        const slashed_file = try alloc.dupeZ(u8, file);
+        defer alloc.free(slashed_file);
+        std.mem.replaceScalar(u8, slashed_file, std.fs.path.sep_windows, std.fs.path.sep_posix);
+
+        const abs_repo_path = try self.repo_root.realpathAlloc(alloc, ".");
+        const file_path_parts = [_][]const u8{ abs_repo_path, file };
+        const abs_path = try std.fs.path.joinZ(alloc, &file_path_parts);
+        defer alloc.free(abs_path);
+
+        log.debug("What is the file name? {s}\n", .{file});
+        log.debug("What is the absolute path? {s}\n", .{abs_path});
+
+        var f_in = try std.fs.openFileAbsolute(abs_path, .{ .lock = .shared });
+        defer f_in.close();
+
+        const f_stat = try f_in.stat();
+        const file_size = f_stat.size;
+        // const file_size: i64 = @intCast(f_stat.size);
+        // const file_size: i64 = f_stat.size;
+
+        var hasher = Hasher.init(null);
+        const hash_algo = @tagName(hasher);
+
+        try fifo.pump(f_in.reader(), hasher.writer());
+
+        var hexz_buf: Hasher.Buffer = undefined;
+        const file_digest_hexz = hasher.hexFinal(&hexz_buf);
+
+        log.debug("blob_hash: {s}\nblob_hash_alg: {s}\nblob_size: {any}\n", .{ file_digest_hexz, hash_algo, file_size });
+        try self.blob_stmt.reset();
+        try self.blob_stmt.clear_bindings();
+        try self.blob_stmt.bind_text(1, false, file_digest_hexz);
+        try self.blob_stmt.bind_text(2, false, hash_algo);
+        try self.blob_stmt.bind(3, false, @as(i64, @intCast(file_size)));
+        try self.blob_stmt.auto_step();
+        try self.blob_stmt.reset();
+        try self.blob_stmt.clear_bindings();
+
+        log.debug("file_path: {s}\nfile_hash: {s}\nfile_hash_alg: {s}\nfile_size: {any}\n", .{ slashed_file, file_digest_hexz, hash_algo, file_size });
+        try self.file_stmt.reset();
+        try self.file_stmt.clear_bindings();
+        try self.file_stmt.bind_text(1, false, slashed_file);
+        try self.file_stmt.bind_text(2, false, file_digest_hexz);
+        try self.file_stmt.bind_text(3, false, hash_algo);
+        try self.file_stmt.bind(4, false, @as(i64, @intCast(file_size)));
+        try self.file_stmt.auto_step();
+        try self.file_stmt.reset();
+        try self.file_stmt.clear_bindings();
+
+        // Rewind to beginning of file before chunking
+        try f_in.seekTo(0);
+
+        var cur_pos: @TypeOf(file_size) = 0;
+        while (cur_pos < file_size) : (cur_pos = try f_in.getPos()) {
+            chunk_buf_stream.reset();
+
+            // const compression_algo: [:0]const u8 = "zstd";
+            const compression_algo: [:0]const u8 = "none";
+
+            var chunk_hasher = Hasher.init(null);
+            const chunk_hash_algo = @tagName(chunk_hasher);
+
+            var mwriter = std.io.multiWriter(.{ chunk_hasher.writer(), chunk_buf_stream.writer() });
+
+            var lr = std.io.limitedReader(f_in.reader(), chunk_size);
+
+            try fifo.pump(lr.reader(), mwriter.writer());
+
+            const true_end_pos = try f_in.getPos();
+            std.debug.assert(true_end_pos > cur_pos);
+
+            const end_pos = true_end_pos - 1;
+
+            var chunk_hexz_buf: Hasher.Buffer = undefined;
+            const chunk_digest_hexz = chunk_hasher.hexFinal(&chunk_hexz_buf);
+
+            const data: []const u8 = chunk_buf_stream.getWritten();
+
+            log.debug("What is the contents of {s}? '{s}'\n", .{ file, chunk_buf_stream.getWritten() });
+            log.debug("What is the hash contents of chunk for {s}? {s}\n", .{ file, chunk_digest_hexz });
+
+            log.debug("blob_hash: {s}, blob_hash_algo: {s}, chunk_hash: {s}, chunk_hash_algo: {s}, start_byte: {any}, end_byte: {any}, compression_algo: {?s}\n", .{ file_digest_hexz, hash_algo, chunk_digest_hexz, hash_algo, cur_pos, end_pos, compression_algo });
+
+            // INSERT INTO "chunks"
+            try self.chunk_stmt.reset();
+            try self.chunk_stmt.clear_bindings();
+            try self.chunk_stmt.bind_text(1, false, chunk_digest_hexz); // chunk_hash
+            try self.chunk_stmt.bind_text(2, false, chunk_hash_algo); // chunk_hash_algo
+            try self.chunk_stmt.bind_text(3, false, compression_algo); // compression_algo
+            try self.chunk_stmt.bind_blob(4, false, data); // data
+            try self.chunk_stmt.auto_step();
+            try self.chunk_stmt.reset();
+            try self.chunk_stmt.clear_bindings();
+
+            // INSERT INTO "blob_chunks"
+            try self.blob_chunk_stmt.reset();
+            try self.blob_chunk_stmt.clear_bindings();
+            try self.blob_chunk_stmt.bind_text(1, false, file_digest_hexz); // blob_hash
+            try self.blob_chunk_stmt.bind_text(2, false, hash_algo); // blob_hash_algo
+            try self.blob_chunk_stmt.bind_text(3, false, chunk_digest_hexz); // chunk_hash
+            try self.blob_chunk_stmt.bind_text(4, false, chunk_hash_algo); // chunk_hash_algo
+            try self.blob_chunk_stmt.bind(5, false, @as(i64, @intCast(cur_pos))); // start_byte
+            try self.blob_chunk_stmt.bind(6, false, @as(i64, @intCast(end_pos))); // end_byte
+            try self.blob_chunk_stmt.auto_step();
+            try self.blob_chunk_stmt.reset();
+            try self.blob_chunk_stmt.clear_bindings();
+
+            // TODO: Add zstd compression
+        }
+
+        try file_sp.commit();
     }
 };
 

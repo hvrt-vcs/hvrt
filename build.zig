1cda5b3ac45bc2c314e443e057d74821e68d7e90
Ethan Estrada <ethan@misterfidget.com>
2025-07-18 23:33:00 -0500

    Move exe compilation out of build function

--- build.zig
+++ build.zig
@@ -145,21 +145,7 @@ fn compilePcre2(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.b
     return pcre2_sl;
 }
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
-
+fn compileExe(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
     const sqlite_sl = compileSqlite(b, target, optimize);
 
     const pcre2_sl = compilePcre2(b, target, optimize);
@@ -181,6 +167,30 @@ pub fn build(b: *std.Build) void {
     // We use c_allocator from libc for allocator implementation, since it is
     // the fastest builtin allocator currently offered by zig.
     exe.linkLibC();
+
+    return exe;
+}
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const sqlite_sl = compileSqlite(b, target, optimize);
+
+    const pcre2_sl = compilePcre2(b, target, optimize);
+
+    const exe = compileExe(b, target, optimize);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
